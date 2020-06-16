a06ecb810cdc07debbe0191147a33df18b31b598
Nilber Remon <nilberremon@gmail.com>
2020-06-16 18:20:21 -0400

    PJB App Deployment Debugging

--- Backend/Routes/Upload/upload.js
+++ Backend/Routes/Upload/upload.js
@@ -1,9 +1,11 @@
 const upload = require("../../multer")
 const uploadRoute = require("express").Router()
 
+const apiURL = require('../../Utilitique/APIURL')
+
 uploadRoute.post("/", upload.single("image"), (req, res, next) => {
     try {
-        let url = `"http://localhost:3000/ImageUploads/${req.file.filename}`;
+        let url = `${apiURL}/ImageUploads/${req.file.filename}`;
         res.json({
             imageUrl: url,
             message: "File Uploaded"
