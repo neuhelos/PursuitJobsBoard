ffb57b4dc2b6d0e6789765fce1c9de9190fd1134
Nilber Remon <nilberremon@gmail.com>
2020-08-03 15:43:44 -0400

    Location Autocomplete

--- pursuit-jobsboard/src/features/AddJobsPosts/LocationAutocompleteInput.js
+++ pursuit-jobsboard/src/features/AddJobsPosts/LocationAutocompleteInput.js
@@ -16,50 +16,55 @@ const LocationSearchInput = () => {
     setLocation(location)
   };
 
-    return (
-      <PlacesAutocomplete
-        value={location}
-        onChange={handleChange}
-        onSelect={handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => {
+  const searchOptions = {
+    types: ['(regions)']
+  }
 
-          return (
-          <>
-          <TextField id="jobLocation" placeholder="Job Location"  label="Job Location" variant="outlined" {...getInputProps()} required/>
-            {/* <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-              required
-            /> */}
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? 'suggestion-item--active'
-                  : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style,
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </>
-        )}}
-      </PlacesAutocomplete>
-    );
+  return (
+    <PlacesAutocomplete
+      value={location}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      searchOptions={searchOptions}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => {
+
+        return (
+        <>
+        <TextField id="jobLocation" placeholder="Job Location"  label="Job Location" variant="outlined" {...getInputProps()} required/>
+          {/* <input
+            {...getInputProps({
+              placeholder: 'Search Places ...',
+              className: 'location-search-input',
+            })}
+            required
+          /> */}
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map(suggestion => {
+              const className = suggestion.active
+                ? 'suggestion-item--active'
+                : 'suggestion-item';
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}}
+    </PlacesAutocomplete>
+  );
 }
 
 export default LocationSearchInput
