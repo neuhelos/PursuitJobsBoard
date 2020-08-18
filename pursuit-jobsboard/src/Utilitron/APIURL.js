18ad8f82e8f89e5a788a8bf40e55f1fcca45e9f1
Nilber Remon <nilberremon@gmail.com>
2020-08-18 03:27:27 -0400

    apiURL Dev-Prod Switch Update

--- pursuit-jobsboard/src/Utilitron/APIURL.js
+++ pursuit-jobsboard/src/Utilitron/APIURL.js
@@ -1,4 +1,4 @@
 export const apiURL = () => {
     return window.location.hostname === "localhost"
-      ? "http://localhost:3000" : "https://pursuitjobsboard.herokuapp.com/";
+      ? "http://localhost:3000" : "https://pursuitjobsboard.herokuapp.com";
   };
