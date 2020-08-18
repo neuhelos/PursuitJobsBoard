0203a64ee08a9fcff5966c9d57223c7bf2e7b29f
Nilber Remon <nilberremon@gmail.com>
2020-08-18 14:18:48 -0400

    Filename Uppercase Fix

--- PJBBackend/Utilitique/apiURL.js
+++
@@ -1,6 +0,0 @@
-const apiURL = () => {
-    return window.location.hostname === "localhost"
-      ? "http://localhost:3000" : "https://pursuitjobsboard.herokuapp.com/";
-  };
-
-module.exports = apiURL
