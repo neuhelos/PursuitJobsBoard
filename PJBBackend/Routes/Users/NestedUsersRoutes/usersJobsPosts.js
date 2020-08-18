136a5b3a01d1acb0b9319bdd86ea38b2aaa979dd
Nilber Remon <nilberremon@gmail.com>
2020-08-18 13:58:06 -0400

    File Uppercase Name

--- PJBBackend/Routes/Users/NestedUsersRoutes/usersJobsPosts.js
+++ PJBBackend/Routes/Users/NestedUsersRoutes/usersJobsPosts.js
@@ -1,7 +1,7 @@
 const usersJobsPosts = require('express').Router({mergeParams: true})
 const {createJobPost, getJobsPostsByUser } = require("../../../Queries/jobsQueries")
 
-//Heroku
+//Heroku Update
 usersJobsPosts.post('/:id/jobs', createJobPost)
 usersJobsPosts.get('/:id/jobs', getJobsPostsByUser)
 
