7b25b07ec16204a5f063ef4d6eb059e69808e1df
Nilber Remon <nilberremon@gmail.com>
2020-08-18 13:44:31 -0400

    Heroku Update

--- PJBBackend/Routes/Users/NestedUsersRoutes/usersJobsPosts.js
+++ PJBBackend/Routes/Users/NestedUsersRoutes/usersJobsPosts.js
@@ -1,6 +1,7 @@
 const usersJobsPosts = require('express').Router({mergeParams: true})
 const {createJobPost, getJobsPostsByUser } = require("../../../Queries/jobsQueries")
 
+//Heroku
 usersJobsPosts.post('/:id/jobs', createJobPost)
 usersJobsPosts.get('/:id/jobs', getJobsPostsByUser)
 
