1c29a88aa85144d44d18ea5078d3e5bc0c67a9cd
Nilber Remon <nilberremon@gmail.com>
2020-10-06 22:56:03 -0400

    App Width Styling

--- pursuit-jobsboard/src/features/JobsBoard/JobsSearchSort.js
+++ pursuit-jobsboard/src/features/JobsBoard/JobsSearchSort.js
@@ -6,51 +6,57 @@ import Switch from '@material-ui/core/Switch';
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles( theme => {
+    
     const pxToRem = (px, oneRemPx = 17) => `${px / oneRemPx}rem`;
     const borderWidth = 2;
     const width = pxToRem(56);
     const height = pxToRem(34);
     const size = pxToRem(22);
     const gap = (34 - 22) / 2;
+    
     return {
-    root: {
-        width,
-        height,
-        padding: 0,
-        overflow: 'unset',
-    },
-    switchBase: {
-        padding: pxToRem(gap),
-        '&$checked': {
-            color: '#fff',
-            transform: `translateX(calc(${width} - ${size} - ${pxToRem(2 * gap)}))`,
-            '& + $track': {
-                backgroundColor: theme.palette.primary.main,
-                opacity: 1,
-                border: 'none',
-            },
-            '& $thumb': {
-                backgroundColor: '#fff',
+
+        container: {
+            width: '100%'
+        },
+        root: {
+            width,
+            height,
+            padding: 0,
+            overflow: 'unset',
+        },
+        switchBase: {
+            padding: pxToRem(gap),
+            '&$checked': {
+                color: '#fff',
+                transform: `translateX(calc(${width} - ${size} - ${pxToRem(2 * gap)}))`,
+                '& + $track': {
+                    backgroundColor: theme.palette.primary.main,
+                    opacity: 1,
+                    border: 'none',
+                },
+                '& $thumb': {
+                    backgroundColor: '#fff',
+                },
             },
         },
-    },
-    track: {
-        borderRadius: 40,
-        border: `solid ${theme.palette.grey[400]}`,
-        borderWidth,
-        backgroundColor: theme.palette.grey[50],
-        opacity: 1,
-        transition: theme.transitions.create(['background-color', 'border']),
-        boxSizing: 'border-box',
-    },
-    thumb: {
-        boxShadow: 'none',
-        backgroundColor: theme.palette.grey[400],
-        width: size,
-        height: size,
-    },
-    checked: {},
-    };
+        track: {
+            borderRadius: 40,
+            border: `solid ${theme.palette.grey[400]}`,
+            borderWidth,
+            backgroundColor: theme.palette.grey[50],
+            opacity: 1,
+            transition: theme.transitions.create(['background-color', 'border']),
+            boxSizing: 'border-box',
+        },
+        thumb: {
+            boxShadow: 'none',
+            backgroundColor: theme.palette.grey[400],
+            width: size,
+            height: size,
+        },
+        checked: {},
+        };
 })
 
 
@@ -60,8 +66,7 @@ const JobsSearchSort = ( {handleChange, sortToggle} ) => {
     const classes = useStyles();
     
     return (
-        <div>
-            <Grid component="label" container alignItems="center" spacing={1}>
+        <Grid className={classes.container} component="label" container alignItems="center" spacing={1}>
             <Grid item>Recently Posted</Grid>
                 <Grid item>
                     <Switch
@@ -71,8 +76,7 @@ const JobsSearchSort = ( {handleChange, sortToggle} ) => {
                     />
                 </Grid>
             <Grid item>Soonest Closing Date</Grid>
-            </Grid>
-        </div>
+        </Grid>
     );
 
 }
